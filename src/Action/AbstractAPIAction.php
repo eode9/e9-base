79cce319441d9685c648956d3f7de737c1fd2793
Kevin Marie <kevin.marie@eode9.com>
2018-05-16 17:17:29 +0200

    initial commit

---
+++ src/Action/AbstractAPIAction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace E9\Core\Action;
+
+use JSend\InvalidJSendException;
+use JSend\JSendResponse;
+use Slim\Http\Response;
+
+/**
+ * Class APIAbstractAction
+ * @package App\Core\Action
+ */
+abstract class AbstractAPIAction
+{
+    /**
+     * @param Response $response
+     * @param $message
+     * @param $data
+     * @param $code
+     * @return Response
+     */
+    public function prepareError(Response $response, $message, $data, $code): Response
+    {
+        try {
+            return $response->withJson(JSendResponse::error($message, $code, $data), $code);
+        } catch (InvalidJSendException $e) {
+            return $response->withJson(['status' => 'error', 'message' => 'Server error'], 500);
+        }
+    }
+
+    /**
+     * @param Response $response
+     * @param $data
+     * @param $code
+     * @return Response
+     */
+    public function prepareSuccess(Response $response, $data, $code): Response
+    {
+        try {
+            return $response->withJson(JSendResponse::success($data), $code);
+        } catch (InvalidJSendException $e) {
+            return $response->withJson(['status' => 'error', 'message' => 'Server error'], 500);
+        }
+    }
+}
---
+++ src/Entity/AbstractEntity.php
@@ -0,0 +1,54 @@
+<?php
+namespace E9\Core\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class AbstractEntity
+ * @package App\Core\Common
+ *
+ * @ORM\MappedSuperclass()
+ */
+abstract class AbstractEntity implements \JsonSerializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="guid", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    public $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", name="created_at", nullable=true)
+     */
+    public $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", name="updated_at", nullable=true)
+     */
+    public $updatedAt;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onPrePersist() : void
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate() : void
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    abstract public function jsonSerialize();
+}
