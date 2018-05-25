4df80a640ea9f6f6d5d4632198eb6948395e7787
Kevin Marie <kevin.marie@eode9.com>
2018-05-25 17:38:09 +0200

    Fixing Lang collection name

--- src/Document/Lang.php
+++ src/Document/Lang.php
@@ -2,11 +2,13 @@
 
 namespace E9\Core\Document;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Collection;
 use Doctrine\ODM\MongoDB\Mapping\Annotations\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations\Field;
 
 /**
  * @Document(repositoryClass="E9\Core\Repository\LangRepository")
+ * @Collection(name="core_lang")
  */
 class Lang extends AbstractDocument
 {
